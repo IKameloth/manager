4f7914aef41759915f9c03407bbca00368cd112d
Juanko <jcbustosj@gmail.com>
2020-11-12 13:45:45 -0300

    change grid for flex

--- src/assets/styled/login.js
+++ src/assets/styled/login.js
@@ -16,41 +16,33 @@ export const Main = styled.main`
   width: 100vw;
   height: 100vh;
   background: #f7f9fc;
-  display: grid;
-  grid-template-columns: 1fr;
+  display: flex;
   animation: 1s ${fadeIn} ease-in;
-  
-  @media screen and (min-width: 40em) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-  @media screen and (min-width: 72em) {
-    grid-template-columns: repeat(12, 1fr);
-  }
 `;
 
 export const H1 = styled.h1`
-  margin-top: 8rem;
-  margin-bottom: 6rem !important;
   span {
     color: #2962ff;
   }
 `;
 
 export const H3 = styled.h3`
-  margin-bottom: 2rem !important;
+
 `;
 
 export const Form = styled.form`
-  grid-column: initial;
   background: #fff;
-  padding-left: 3.125vw;
-  padding-right: 3vw;
-
-  @media screen and (min-width: 40em) {
-    grid-column: 1 / 3;
-  }
-  @media screen and (min-width: 72em) {
-    grid-column: 1 / span 3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 30%;
+  .content{
+    width: 80%;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
   }
 `;
 
@@ -58,27 +50,11 @@ export const LoginImage = styled.img.attrs(props => ({
   src: BackgroundImage,
   alt: "Login Image",
 }))`
-  width: 100%;
-  grid-column: initial;
+  width: 100vw;
   align-self: center;
-  display: none;
-  
-  @media screen and (min-width: 40em) {
-    grid-column: 3 / 5;
-    display: inline;
-    object-fit: cover;
-    height: 70%;
-    object-position: 2vw;
-  }
-  @media screen and (min-width: 72em) {
-    grid-column: 4 / last-line;
-    display: inline;
-  }
 `;
 
 export const Forgot = styled.a`
-  margin-top: 0.5rem;
-  margin-bottom: 2.5rem;
   display: inline-block;
 `;
 
@@ -91,5 +67,4 @@ export const LogoImage = styled.img.attrs(props => ({
 `;
 
 export const SectionLink = styled.div`
-  margin-bottom: 12rem !important;
 `;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,48 +12,52 @@ const Login = () => {
       <ProgressBar />
       <Main>
         <Form>
-          <H1 className="title is-1">Autentia<span> Home</span></H1>
-          <H3 className="title is-3">{t('title.label')}</H3>
-          <div className="field">
-            <label className="label">{t('dni.label')}</label>
-            <div className="control has-icons-left has-icons-right">
-              <input className="input is-danger" type="email" placeholder={`${t('dni.placeholder')}`}/>
-              <span className="icon is-small is-left">
-                <i className="fas fa-envelope"></i>
-              </span>
-              <span className="icon is-small is-right">
-                <i className="fas fa-exclamation-triangle"></i>
-              </span>
+          <div className="content">
+            <H1 className="title is-1">Autentia<span> Admin</span></H1>
+            <H3 className="title is-3">{t('title.label')}</H3>
+            <div className="fields">
+              <div className="field">
+                <label className="label">{t('dni.label')}</label>
+                <div className="control has-icons-left has-icons-right">
+                  <input className="input is-danger" type="email" placeholder={`${t('dni.placeholder')}`}/>
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-envelope"></i>
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                </div>
+                <p className="help is-danger">{t('dni.invalid')}</p>
+              </div>
+
+              <div className="field">
+                <label className="label">{t('password.label')}</label>
+                <div className="control has-icons-left has-icons-right">
+                  <input className="input is-danger" type="password" placeholder={`${t('password.placeholder')}`}/>
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-fingerprint"></i>
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                </div>
+                <p className="help is-danger">{t('password.invalid')}</p>
+              </div>
             </div>
-            <p className="help is-danger">{t('dni.invalid')}</p>
-          </div>
 
-          <div className="field">
-            <label className="label">{t('password.label')}</label>
-            <div className="control has-icons-left has-icons-right">
-              <input className="input is-danger" type="password" placeholder={`${t('password.placeholder')}`}/>
-              <span className="icon is-small is-left">
-                <i className="fas fa-fingerprint"></i>
-              </span>
-              <span className="icon is-small is-right">
-                <i className="fas fa-exclamation-triangle"></i>
-              </span>
+            <LanguajeSelector/>
+            <Forgot href="/">{`${t('password.forget')}`}</Forgot>
+
+            <SectionLink className="field">
+              <div className="control">
+                <Link to="/index" className="button is-link">{t('btn.login')}</Link>
+              </div>
+            </SectionLink>
+
+            <div>
+              <LogoImage/>
+              <span>Copyright 2020</span>
             </div>
-            <p className="help is-danger">{t('password.invalid')}</p>
-          </div>
-
-          <LanguajeSelector/>
-          <Forgot href="/">{`${t('password.forget')}`}</Forgot>
-
-          <SectionLink className="field">
-            <div className="control">
-              <Link to="/index" className="button is-link">{t('btn.login')}</Link>
-            </div>
-          </SectionLink>
-
-          <div>
-            <LogoImage/>
-            <span>Copyright 2020</span>
           </div>
         </Form>
         <LoginImage/>
