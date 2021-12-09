8378c54604d68e56ac254524a7ae4980ea353d43
Juanko <jcbustosj@gmail.com>
2021-12-09 15:10:44 -0300

    add config by ambient

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,19 +1,22 @@
 const dev = {
-  API_URI: process.env.LEGACY_API_URL,
+  REACT_APP_DOMAIN: "https://autentia-admin-dev.autentia.io",
+  API_URI: "https://api-dev.autentiaplus.id/_alpha-legacy-api"
 };
 
 const stg = {
-  API_URI: process.env.LEGACY_API_URL,
+  REACT_APP_DOMAIN: "https://autentia-admin-staging.autentia.io",
+  API_URI: "https://api-staging.autentiaplus.id/_alpha-legacy-api"
 };
 
 const prod = {
-  API_URI: process.env.LEGACY_API_URL,
+  REACT_APP_DOMAIN: "https://autentia-admin.autentia.io",
+  API_URI: "https://api.autentiaplus.id/_alpha-legacy-api"
 };
 
 export default window.location.href.includes('dev') 
   || window.location.href.includes('localhost') 
   || window.location.href.includes('127.0.0.1')  
     ? dev 
-    : window.location.href.includes('stg') 
+    : window.location.href.includes('staging') 
       ? stg 
       : prod
