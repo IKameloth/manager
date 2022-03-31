a65be4456e2d926d26a88e9adb63463348989d4c
Juanko <jcbustosj@gmail.com>
2022-03-31 09:52:02 -0300

    add js lib in public index

--- src/app/pages/people/index.tsx
+++ src/app/pages/people/index.tsx
@@ -19,7 +19,6 @@ import SearchIcon from "@material-ui/icons/Search";
 import { MotionContainer, MotionItemUp } from "@/app/components/Motion";
 import { useForm, SubmitHandler } from "react-hook-form";
 import {
-  AutentiaHeaderResources,
   autentiaTrxCaller,
 } from "@/app/components/Autentia";
 import { TrxList } from "@/app/helper/TrxList";
@@ -74,78 +73,75 @@ export default function People() {
   const handleTest = () => {};
 
   return (
-    <>
-      <AutentiaHeaderResources />
-      <Container>
-        <Section>
-          <Grid container spacing={8}>
-            <TitleBar
-              title="TESTING"
-              subTitle="plugin autentia"
-              btnText="test"
-              btnAction={handleTest}
-            />
-            <Box
-              alignItems="center"
-              justifyContent="center"
-              display="flex"
-              width="100%"
-            >
-              <Grid item xs={12} sm={6} md={8} lg={4}>
-                <MotionContainer>
-                  <Card>
-                    <CardHeader title="Verificacion contra Base" />
-                    <CardContent>
-                      <MotionItemUp>
-                        <form onSubmit={handleSubmit(sendForm)}>
-                          <FormControl
-                            variant="outlined"
-                            fullWidth
-                            error={errors.dni ? true : false}
-                          >
-                            <InputLabel htmlFor="dni">Ingresar DNI</InputLabel>
-                            <OutlinedInput
-                              id="dni"
-                              label="Ingresar dni"
-                              type="text"
-                              autoFocus
-                              {...register("dni", {
-                                required: true,
-                                validate: {
-                                  required: (value) => !!value.trim().length,
-                                },
-                              })}
-                            />
-                            <FormHelperText id="dni-error">
-                              {errors.dni && errors.dni.type === "required" && (
-                                <span>Campo requerido</span>
-                              )}
-                            </FormHelperText>
-                          </FormControl>
+    <Container>
+      <Section>
+        <Grid container spacing={8}>
+          <TitleBar
+            title="TESTING"
+            subTitle="plugin autentia"
+            btnText="test"
+            btnAction={handleTest}
+          />
+          <Box
+            alignItems="center"
+            justifyContent="center"
+            display="flex"
+            width="100%"
+          >
+            <Grid item xs={12} sm={6} md={8} lg={4}>
+              <MotionContainer>
+                <Card>
+                  <CardHeader title="Verificacion contra Base" />
+                  <CardContent>
+                    <MotionItemUp>
+                      <form onSubmit={handleSubmit(sendForm)}>
+                        <FormControl
+                          variant="outlined"
+                          fullWidth
+                          error={errors.dni ? true : false}
+                        >
+                          <InputLabel htmlFor="dni">Ingresar DNI</InputLabel>
+                          <OutlinedInput
+                            id="dni"
+                            label="Ingresar dni"
+                            type="text"
+                            autoFocus
+                            {...register("dni", {
+                              required: true,
+                              validate: {
+                                required: (value) => !!value.trim().length,
+                              },
+                            })}
+                          />
+                          <FormHelperText id="dni-error">
+                            {errors.dni && errors.dni.type === "required" && (
+                              <span>Campo requerido</span>
+                            )}
+                          </FormHelperText>
+                        </FormControl>
 
-                          <CardActions
-                            style={{ padding: 16, justifyContent: "center" }}
+                        <CardActions
+                          style={{ padding: 16, justifyContent: "center" }}
+                        >
+                          <Fab
+                            variant="extended"
+                            color="primary"
+                            onClick={handleSubmit(sendForm)}
+                            size="medium"
                           >
-                            <Fab
-                              variant="extended"
-                              color="primary"
-                              onClick={handleSubmit(sendForm)}
-                              size="medium"
-                            >
-                              <SearchIcon />
-                              Consultar
-                            </Fab>
-                          </CardActions>
-                        </form>
-                      </MotionItemUp>
-                    </CardContent>
-                  </Card>
-                </MotionContainer>
-              </Grid>
-            </Box>
-          </Grid>
-        </Section>
-      </Container>
-    </>
+                            <SearchIcon />
+                            Consultar
+                          </Fab>
+                        </CardActions>
+                      </form>
+                    </MotionItemUp>
+                  </CardContent>
+                </Card>
+              </MotionContainer>
+            </Grid>
+          </Box>
+        </Grid>
+      </Section>
+    </Container>
   );
 }
