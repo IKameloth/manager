6e55127226cc0619110ecbfa51ffc2da182806e0
Juanko <jcbustosj@gmail.com>
2022-04-04 14:24:30 -0400

    fix public path in docker

--- Dockerfile
+++ Dockerfile
@@ -19,6 +19,7 @@ RUN yarn build
 FROM nginx:stable as final
 WORKDIR /web
 COPY ./nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=build-react /app/public/assets /usr/share/nginx/html/assets
 COPY --from=build-react /app/dist /usr/share/nginx/html
 
 # CMD npm start
--- webpack.common.js
+++ webpack.common.js
@@ -57,11 +57,6 @@ module.exports = {
         }
       },
       {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
-      },
-      {
         test: /\.(json)$/i,
         loader: 'file-loader',
         options: {
